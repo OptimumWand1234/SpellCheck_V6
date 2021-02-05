3c2f9e6c35fa963f97cd1aa808a72da59cc1c10a
AryanJain <JainAryan@hotmail.co.uk>
2021-02-05 16:03:02 +0000

    Fourth

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 
 public class Main {
-    //hello
     public static void main(String[] args) throws IOException {
         MainMethodsForMainToRun mainToRunProgram = new MainMethodsForMainToRun();
         mainToRunProgram.mainMethods();
